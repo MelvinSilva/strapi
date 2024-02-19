6ab9b4bf1a23ee8d53ca1b5f209d31ef23a046b8
Melvin SILVA <melvin.silva@sncf.fr>
2024-02-19 19:12:35 +0100

    change value field valide, value = null defaut

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -763,7 +763,7 @@ export interface ApiTipTip extends Schema.CollectionType {
     reduction: Attribute.String;
     image: Attribute.Media;
     verifier: Attribute.Boolean;
-    valide: Attribute.Boolean & Attribute.Required & Attribute.DefaultTo<true>;
+    valide: Attribute.Boolean;
     adresse: Attribute.Text;
     date_verification: Attribute.String;
     tag: Attribute.String;
